f32c15caa4fcf9ef619d49693b89a785a9b4d1dc
meganemura <meganemura@users.noreply.github.com>
2019-09-03 22:43:42 +0900

    Implement PrefixedDelegation

---
+++ lib/prefixed_delegation/methods.rb
@@ -0,0 +1,15 @@
+module PrefixedDelegation
+  module Methods
+    def delegate(*methods, to: nil, prefix: nil, allow_nil: nil, private: nil, prefixed: nil)
+      return super unless to
+      return super unless prefixed
+
+      method_prefixed_by = "#{prefixed == true ? to : prefixed}_"
+      substituted_methods = methods.map { |x| x.to_s.sub(/^#{method_prefixed_by}/, '').to_sym }
+
+      super(*substituted_methods, to: to, prefix: prefixed, allow_nil: allow_nil, private: private)
+    end
+  end
+end
+
+Module.prepend(::PrefixedDelegation::Methods)
--- spec/prefixed_delegation_spec.rb
+++ spec/prefixed_delegation_spec.rb
@@ -1,9 +1,32 @@
 RSpec.describe PrefixedDelegation do
-  it "has a version number" do
-    expect(PrefixedDelegation::VERSION).not_to be nil
+  class User
+    def first_name
+      'John'
+    end
+
+    def last_name
+      'Doe'
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  class Company
+    delegate :user_first_name, to: :user, prefixed: true
+    delegate :john_last_name, to: :user, prefixed: :john
+
+    def user
+      User.new
+    end
+  end
+
+  describe 'delegate with prefixed option' do
+    it 'delegates successfully with prefixed: true' do
+      company = Company.new
+      expect(company.user_first_name).to eq('John')
+    end
+
+    it 'delegates successfully with prefixed: symbol' do
+      company = Company.new
+      expect(company.john_last_name).to eq('Doe')
+    end
   end
 end
